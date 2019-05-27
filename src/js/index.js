00dc6747fcab8f482afe3ba0a2dc8a32a178c60e
Excuse me <wz666s@163.com>
2019-05-27 16:16:27 +0800

    add

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import Cool from './components/cool';
 import alertinfo from './components/debug/alertinfo';
 import Pajax from 'pajax';
 $(document).ready(() => {
-  if(token){
+  if (token) {
     // document.body.onbeforeunload = function(event) {
     //   var c = event || window.event;
     //   if (/webkit/.test(navigator.userAgent.toLowerCase())) {
@@ -26,5 +26,31 @@ $(document).ready(() => {
     });
     return serializeObj;
   };
+  var param = {
+      type: 0,
+      key: ""
+    },
+    changeUrl;
+  $('body').off('click', '.search_bar .search span').on('click', '.search_bar .search span', function () {
+    $('.search_bar .search ul').toggleClass('on');
+  })
+  $('body').off('click', '.search_bar .search ul a').on('click', '.search_bar .search ul a', function () {
+    var id = $(this).data('id'),
+      text = $(this).html();
+    $('.search_bar .search ul').removeClass('on');
+    $('.search_bar .search span').html(text + '<i class="fa fa-caret-down"></i>');
+    $('.search_bar .search span').data('id', id);
+    param.type = id;
+    changeUrl()
+  })
+  $('body').off('input propertychange', '.search_bar .search input').on('input propertychange', '.search_bar .search input', function () {
+    var e = $(this).val();
+    param.key = e;
+    changeUrl()
+  })
+  changeUrl = function () {
+    var url = $('.search_bar .search>a').data('url');
+    $('.search_bar .search>a').attr('href', url + '?type=' + param.type + '&key=' + param.key);
+  }
   const cool = new Cool();
 });
